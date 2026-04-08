694c986fc2549560da1c0e990d9aa38df53779d7
Trepository-Arepository <109420023+Tunip-Anip@users.noreply.github.com>
2026-04-08 13:20:22 +1000

    Fixed the codex title screen
    YAY

--- Site/script.js
+++ Site/script.js
@@ -3,9 +3,17 @@ var pages = ["index.html","Lorem.html","",]
 
 
 function changeImage() {
-    if (document.getElementById("Codex-btn").src == "CodexStill.png"){
-        document.getElementById("Codex-btn").src = "Codex.gif";
-
-    }
+    console.log("hewo")
+    document.getElementById("Codex-btn").src = "CodexContinuos.gif";
+    setTimeout(slowgif, 3000)
+        
 }
-  
+
+function slowgif(){
+    document.getElementById("Codex-btn").src = "Codex.gif";
+    setTimeout(stopgif, 450)
+}
+
+function stopgif(){
+    document.getElementById("Codex-btn").src = "CodexFreeze.png";
+}
