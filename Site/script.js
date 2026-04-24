9b51c17a2ba6a83de8a5e837baa51ac95e21d39b
Trepository-Arepository <109420023+Tunip-Anip@users.noreply.github.com>
2026-04-24 12:43:31 +1000

    Fixed everything

--- Site/script.js
+++ Site/script.js
@@ -1,6 +1,17 @@
-var pages = ["index.html","Lorem.html","",]
+var pages = ["gamescreen.html","",]
+var symbols = ["Pentacle","Fib","Golden_Ratio","Hindu_Swastika"]
+const Pentacle = {
+  Name: "Pentacle",
+  info: "The Pentacle; a five pronged star enclosed in a circle was a heavily appreciated symbol around ancient human history, through multiple religions it has been claimed to symbolise many different things such as the five elements: Wood, Fire, Water, Earth and Metal in Daoism which was a philisophical chinese point of view, in other cultures it is said to represent the five human senses, but most of them converge to symbols of love, life and beauty, follwoing goddess rituals envoking and reestablishing the natural Femininity of the world.",
+  OriginDate: "3500 BCE",
+  OriginPlace: "Mesopotamia",
+  ReligousGroup: "Polytheism",
+  image: "Pentacle.png"
+};
 
-
+function next(){
+      document.location.href = pages[0];
+    }
 
 function changeImage() {
     console.log("hewo")
@@ -16,4 +27,18 @@ function slowgif(){
 
 function stopgif(){
     document.getElementById("Codex-btn").src = "CodexFreeze.png";
+    setTimeout(next, 450)
+
+    
 }
+
+function newPopup(url,symbol) {
+    
+	popupWindow = window.open(
+    url,'popUpWindow','height=300,width=400,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
+    document.getElementById("changesymbol").src = "Pentacle.png";
+    
+}   
+
+
+    
