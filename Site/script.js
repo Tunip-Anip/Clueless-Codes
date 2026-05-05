af4d6468e1e7d999151cbd6e996ac42a1bd32b98
Trepository-Arepository <109420023+Tunip-Anip@users.noreply.github.com>
2026-05-05 19:49:51 +1000

    Commit quick
    ty sidd

--- Site/script.js
+++ Site/script.js
@@ -1,6 +1,6 @@
 var pages = ["gamescreen.html","",]
 var symbols = ["Pentacle","Fib","Golden_Ratio","Hindu_Swastika"]
-let symbolc = ""
+let symbolc = "";
 const Pentacle = {
   Name: "Pentacle",
   info: "The Pentacle; a five pronged star enclosed in a circle was a heavily appreciated symbol around ancient human history, through multiple religions it has been claimed to symbolise many different things such as the five elements: Wood, Fire, Water, Earth and Metal in Daoism which was a philosophical Chinese point of view, in other cultures it is said to represent the five human senses, but most of them converge to symbols of love, life and beauty, following goddess rituals envoking and reestablishing the natural Femininity of the world. Its history ties in well with the pentagram whereas in the renaissance Heinrich Cornelius Agrippa and others portrayed its power to be a symbol for magic. From this it was also used as a source of protection from evil forces, but its meaning as well as the pentagram have been shifted from the protective and earthly symbol that it was to a more demonic and devilish symbol brought upon by occult practices by Judeo-Christian beliefs, where they used the pentacle and pentagram to support their practices which the Christian church would spell heresy.",
@@ -12,8 +12,8 @@ const Pentacle = {
 
 const Fib = {
   Name: "Fibonacci Sequence",
-  info: "The Pentacle; a five pronged star enclosed in a circle was a heavily appreciated symbol around ancient human history, through multiple religions it has been claimed to symbolise many different things such as the five elements: Wood, Fire, Water, Earth and Metal in Daoism which was a philosophical Chinese point of view, in other cultures it is said to represent the five human senses, but most of them converge to symbols of love, life and beauty, following goddess rituals envoking and reestablishing the natural Femininity of the world. Its history ties in well with the pentagram whereas in the renaissance Heinrich Cornelius Agrippa and others portrayed its power to be a symbol for magic. From this it was also used as a source of protection from evil forces, but its meaning as well as the pentagram have been shifted from the protective and earthly symbol that it was to a more demonic and devilish symbol brought upon by occult practices by Judeo-Christian beliefs, where they used the pentacle and pentagram to support their practices which the Christian church would spell heresy.",
-  OriginDate: "100 BCE - 350 CE",
+  info: "The fibonacci sequence, while being a popular sequence with a recognisable name, was not in fact discovered  by fibonacci. It was initially discovered by Indian scholars with Indian mathematics around 400 years before Fibonacci. The first reference to the “Fibonacci Sequence” was with Fibonacci's book e Book of Calculation, (1202). The Fibonacci sequence was a regular number sequence except the next number is the sum of the two last numbers. E.g 0 1 1 2 3 5 8… The Fibonacci Sequence was influenced by the golden ratio and is expressed with the area of the golden rectangle (the rectangle which follows the golden ratio with length: width) following the fibonacci sequence. Expressed by the symbol in the Golden Ratio’s entry",
+  OriginDate: "100 BCE-350 CE",
   OriginPlace: "India",
   ReligousGroup: "Mathematics",
   image: "Fib.png"
@@ -22,7 +22,7 @@ const Fib = {
 const Golden_Ratio = {
   Name: "Golden Ratio",
   info: "The Pentacle; a five pronged star enclosed in a circle was a heavily appreciated symbol around ancient human history, through multiple religions it has been claimed to symbolise many different things such as the five elements: Wood, Fire, Water, Earth and Metal in Daoism which was a philosophical Chinese point of view, in other cultures it is said to represent the five human senses, but most of them converge to symbols of love, life and beauty, following goddess rituals envoking and reestablishing the natural Femininity of the world. Its history ties in well with the pentagram whereas in the renaissance Heinrich Cornelius Agrippa and others portrayed its power to be a symbol for magic. From this it was also used as a source of protection from evil forces, but its meaning as well as the pentagram have been shifted from the protective and earthly symbol that it was to a more demonic and devilish symbol brought upon by occult practices by Judeo-Christian beliefs, where they used the pentacle and pentagram to support their practices which the Christian church would spell heresy.",
-  OriginDate: "500 - 300 BCE",
+  OriginDate: "500-300 BCE",
   OriginPlace: "Ancient Greece",
   ReligousGroup: "Science",
   image: "gRatio.png"
@@ -31,7 +31,7 @@ const Golden_Ratio = {
 const Hindu_Swastika = {
   Name: "Hindu Swastika",
   info: "The Pentacle; a five pronged star enclosed in a circle was a heavily appreciated symbol around ancient human history, through multiple religions it has been claimed to symbolise many different things such as the five elements: Wood, Fire, Water, Earth and Metal in Daoism which was a philosophical Chinese point of view, in other cultures it is said to represent the five human senses, but most of them converge to symbols of love, life and beauty, following goddess rituals envoking and reestablishing the natural Femininity of the world. Its history ties in well with the pentagram whereas in the renaissance Heinrich Cornelius Agrippa and others portrayed its power to be a symbol for magic. From this it was also used as a source of protection from evil forces, but its meaning as well as the pentagram have been shifted from the protective and earthly symbol that it was to a more demonic and devilish symbol brought upon by occult practices by Judeo-Christian beliefs, where they used the pentacle and pentagram to support their practices which the Christian church would spell heresy.",
-  OriginDate: "10,000 to 17,000 BCE and 3000 BCE",
+  OriginDate: "3000 BCE",
   OriginPlace: "Indus Valley",
   ReligousGroup: "Polytheism",
   image: "Hindus.png"
@@ -67,17 +67,46 @@ function newPopup(url,symbol) {
 	popupWindow = window.open(
     url,'popUpWindow','height=300,width=400,left=10,top=10,resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no')
     popupWindow.resizeTo(1129,700)
+
+    
+    
 }   
 
-function changeSymbol() {
+
+
+function changeSymbol(symbo) {
     console.log(symbolc)
     if(symbolc==''){
-        document.getElementById("changesymbol").src = "Pentacle.png";
+        document.getElementById("changesymbol").src = Pentacle.image;
         document.getElementById("Info").innerHTML = Pentacle.info;
         document.getElementById("Ttl").innerHTML = Pentacle.Name;
         document.getElementById("Date").innerHTML = Pentacle.OriginDate;
         document.getElementById("Area").innerHTML = Pentacle.OriginPlace;
         document.getElementById("Group").innerHTML = Pentacle.ReligousGroup;
+    }
+    if(symbolc==''){
+        document.getElementById("changesymbol").src = Fib.image;
+        document.getElementById("Info").innerHTML = Fib.info;
+        document.getElementById("Ttl").innerHTML = Fib.Name;
+        document.getElementById("Date").innerHTML = Fib.OriginDate;
+        document.getElementById("Area").innerHTML = Fib.OriginPlace;
+        document.getElementById("Group").innerHTML = Fib.ReligousGroup;
+    }
+    if(symbolc==''){
+        document.getElementById("changesymbol").src = Golden_Ratio.image;
+        document.getElementById("Info").innerHTML = Golden_Ratio.info;
+        document.getElementById("Ttl").innerHTML = Golden_Ratio.Name;
+        document.getElementById("Date").innerHTML = Golden_Ratio.OriginDate;
+        document.getElementById("Area").innerHTML = Golden_Ratio.OriginPlace;
+        document.getElementById("Group").innerHTML = Golden_Ratio.ReligousGroup;
+    }
+    if(symbolc==''){
+        document.getElementById("changesymbol").src = Hindu_Swastika.image;
+        document.getElementById("Info").innerHTML = Hindu_Swastika.info;
+        document.getElementById("Ttl").innerHTML = Hindu_Swastika.Name;
+        document.getElementById("Date").innerHTML = Hindu_Swastika.OriginDate;
+        document.getElementById("Area").innerHTML = Hindu_Swastika.OriginPlace;
+        document.getElementById("Group").innerHTML = Hindu_Swastika.ReligousGroup;
     }
 }
 
